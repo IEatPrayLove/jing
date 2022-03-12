911974677cb4c08e8377030ce04e685e1b35fb6e
wangyu <13100684032@163.com>
2022-03-12 20:20:56 +0800

    更新

--- pc_new/src/views/main/main.vue
+++ pc_new/src/views/main/main.vue
@@ -117,9 +117,83 @@
         </div>
       </div>
       <div class="home_new_banner">
-        666
+        <div class="home_new_banner_modal"></div>
+        <div class="home_new_1 home_new_item animate__animated" :class="{animate__backInLeft:home_new_banner_show}">
+          <div class="num_bold">1</div>
+          <div class="home_new_item_text">数据标签多达100多种，任意创建，多角度呈现店铺亮点，有效提升30%达人合作意向</div>
+        </div>
+        <div class="home_new_2 animate__animated " :class="{   animate__backInLeft:home_new_banner_show}">
+          <div class="num_bold">2</div>
+           <div class="home_new_2_text">
+             <div>小程序搭建，开发部署全程服务</div>
+             <div>原生开发，快速版本迭代</div>
+             <div>行业一线，专业开发团队</div>
+           </div>
+        </div>
+        <div class="home_new_3 animate__animated animate__delay-1s" :class="{animate__backInDown:home_new_banner_show}">
+          <h2>鲸妙 </h2>
+          <h2 style="color:#4682B4">—笔生花</h2>
+        </div>
+        <div class="home_new_5 animate__animated animate__delay-1s" :class="{animate__backInUp:home_new_banner_show}">
+          <div class="num_bold">3</div>
+          <div class="home_new_item_text">商家独立小程序适用不同场景，多渠道铺货分发，私域分发运营长期锁定带货渠道，减少沟通成本有效复播长期经营</div>
+        </div>
+        <div class="home_new_4 animate__animated animate__delay-1s" :class="{animate__swing:home_new_banner_show}">
+          <div class="num_bold">4</div>
+          <div class="home_new_item_text">彻底告别传统货盘表发单模式，达人合作有效建联，快递提升50%办公效率</div>
+        </div>
+        <div class="home_new_6">
+          <div class="show_modal">
+            <div class="img_modal">
+              <img  class="weapp_icon" src="../../assets/home/weapp.png" alt="">
+            </div>
+            <div class="bottom">
+              <div class="show_text">演示版</div>
+            <div class="line"></div>
+            <div class="samll_icon"></div>
+            </div>
+          </div>
+        </div>
+        <div class="home_new_7">
+          <img class="home_new_logo weibo" src="../../assets/home/weibo.png" alt="">
+          <img class="home_new_logo douyin" src="../../assets/home/douyin.png" alt="">
+          <img class="home_new_logo wechat" src="../../assets/home/wechat.png" alt="">
+          <img class="home_new_logo hongshu" src="../../assets/home/hongshu.png" alt="">
+          <img class="home_new_logo vieo" src="../../assets/home/vieo.png" alt="">
+          <img class="home_new_logo ab" src="../../assets/home/ab.png" alt="">
+          <img class="home_new_logo taobao" src="../../assets/home/taobao.png" alt="">
+          <img class="home_new_logo jingdong" src="../../assets/home/jingdong.png" alt="">
+          <img class="home_new_logo fast_hand" src="../../assets/home/fast_hand.png" alt="">
+        </div>
       </div>
-      <div class="item-router" v-for="item in routerList" :key="item.id">
+      <!-- <div class="item-router" v-for="item in routerList" :key="item.id">
+        <div>
+          <div>{{ item.name }}</div>
+          <div>{{ item.title }}</div>
+        </div>
+        <div class="item-name">
+          <div v-for="item2 in item.list" :key="item2.uid">
+            <div>鲸大大 {{ item2.name }}</div>
+            <div>{{ item2.title }}</div>
+            <div @click="gorouter(item2.url,item2.id)" style="marginTop:60px">
+              <div></div>
+              <div>立即查看</div>
+              <div></div>
+            </div>
+          </div>
+        </div>
+      </div> -->
+      <!-- <div class="item">
+        <div>
+          <img src="@/assets/home/2.png" style="width: 100%" />
+        </div>
+      </div> -->
+      <div class="item">
+        <div>
+          <div></div>
+        </div>
+      </div>
+       <div class="item-router" v-for="item in routerList" :key="item.id">
         <div>
           <div>{{ item.name }}</div>
           <div>{{ item.title }}</div>
@@ -136,16 +210,11 @@
           </div>
         </div>
       </div>
-      <div class="item">
-        <div>
-          <img src="@/assets/home/2.png" style="width: 100%" />
-        </div>
-      </div>
-      <div class="item" style="margin-top: 30px">
+      <!-- <div class="item" style="margin-top: 30px">
         <div>
           <img src="@/assets/home/3.png" style="width: 100%" />
         </div>
-      </div>
+      </div> -->
       <el-dialog
         title=""
         :visible.sync="centerDialogVisible"
@@ -165,6 +234,7 @@
 </template>
 <script>
 import Banner from "@/components/Banner/index.vue";
+import 'animate.css';
 export default {
   components: { Banner },
   data() {
@@ -173,6 +243,7 @@ export default {
       user: JSON.parse(localStorage.getItem("user")),
       centerDialogVisible: false,
       info: {},
+      home_new_banner_show:false,
       routerList: [
         {
           id: 0,
@@ -222,6 +293,17 @@ export default {
     };
   },
   mounted() {
+    document.addEventListener('scroll',(e)=>{
+      var tarEl = document.getElementsByClassName('home_new_banner')[0]
+      const offset = tarEl.getBoundingClientRect();
+      const offsetTop = offset.top;
+      const offsetBottom = offset.bottom;
+      if(offsetTop <= window.innerHeight && offsetBottom >=0){
+        this.home_new_banner_show = true
+      }else{
+        this.home_new_banner_show = false
+      }
+    })
     this.$axios({
       url: "Plblics/customIndex",
       method: "get",
@@ -274,7 +356,7 @@ export default {
 .desc {
 }
 .item-router {
-  margin-top: 100px;
+  margin-top: 50px;
   > div:nth-of-type(1) {
     display: flex;
     align-items: baseline;
@@ -292,7 +374,7 @@ export default {
       margin-right: 20px;
     }
     > div {
-      border: 8px solid #90a7d2;
+      border: 8px solid #547BB3;
       border-radius: 8px;
       padding: 16px;
       width: 46%;
@@ -313,20 +395,20 @@ export default {
         > div:nth-of-type(1) {
           width: 30px;
           height: 100%;
-          background: #90a7d2;
+          background: #547BB3;
           border-radius: 50% 0 0 50%;
           border-right: 2px solid #fff;
         }
         > div:nth-of-type(2) {
           line-height: 40px;
           text-indent: 10px;
-          background: #90a7d2;
+          background: #547BB3;
           color: #fff;
         }
         > div:nth-of-type(3) {
           width: 30px;
           height: 100%;
-          background: #90a7d2;
+          background: #547BB3;
           border-radius: 0 60% 60% 0;
         }
       }
@@ -385,8 +467,210 @@ export default {
 }
 .home_new_banner{
   height: 500px;
-  background-color: aqua;
-  opacity: .3;
+  background-image: url('../../assets/home/home_new_bg.png');
+  background-size: 100% 100%;
+  background-repeat: no-repeat;
+  position: relative;
+  color: #d7d7d9;
+  .home_new_banner_modal{
+    width: 100%;
+    height: 100%;
+    background-color: #000;
+    opacity: .3;
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+  .home_new_item{
+    box-sizing: border-box;
+    padding: 10px;
+    width: 220px;
+    // height: 130px;
+    border-radius: 20px;
+    background-color: rgba(37, 52, 75, .8);
+    display: flex;
+    flex-direction: column;
+    position: absolute;
+    top: 50px;
+    left: 100px;
+  }
+  .home_new_2{
+    width: 300px;
+    // height: 150px;
+    background-color: rgba(37, 52, 75, .8);
+    position: absolute;
+    top: 310px;
+    left: 50px;
+    border-radius: 20px;
+    padding: 25px;
+    line-height: 24px;
+    display: flex;
+    flex-direction: column;
+  }
+  .home_new_2_text{
+    margin-top: 20px;
+    // padding: 0 15px;
+    line-height: 24px;
+  }
+  .home_new_3{
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    // bottom: 0;
+    margin: auto;
+    height: 60px;
+    width: 280px;
+    justify-content: space-between;
+    display: flex;
+    
+    background-color: rgba(37, 52, 75, .8);
+    border-radius: 8px;
+    align-items: center;
+    padding: 20px 30px;
+    font-size: 22px;
+  }
+  .home_new_4{
+    position: absolute;
+    top: 70px;
+    right: 28%;
+    width: 500px;
+    // height: 120px;
+    box-sizing: border-box;
+    // padding: 10px;
+    background-color: rgba(37, 52, 75, .8);
+    border-radius: 20px;
+  }
+  .home_new_5{
+    position: absolute;
+    top: 260px;
+    left: 35%;
+    width: 220px;
+    // height: 180px;
+    box-sizing: border-box;
+    // padding: 10px;
+    background-color: rgba(37, 52, 75, .8);
+    border-radius: 20px;
+  }
+  .home_new_6{
+    position: absolute;
+    top: 260px;
+    left: 53%;
+    .show_modal{
+      display: flex;
+      flex-direction: column;
+      justify-content: flex-end;
+      width: 200px;
+      height: 200px;
+      align-items: center;
+      &:hover>.img_modal{
+        visibility:visible;
+          transform: translate3d(0,0,0);
+          // display: block;
+          opacity: 1;
+      }
+    }
+    .img_modal{
+      transform: translate3d(0,150%,0);
+      visibility: hidden;
+      // display: none;
+      opacity: 0;
+      transition: all .4s cubic-bezier(0.36, 0.66, 0.04, 1);
+    }
+    .weapp_icon{
+      width: 160px;
+      height: 160px;
+    }
+    .bottom{
+      width: 100px;
+      display: flex;
+      flex-direction: row;
+      justify-content: center;
+      border: 1px solid #fff;
+      box-sizing: border-box;
+      padding: 5px;
+    }
+    .samll_icon{
+      width: 20px;
+      height: 20px;
+      background-image: url('../../assets/home/small_icon.png');
+      background-size: 100% 100%;
+      background-repeat: no-repeat;
+    }
+    .show_text{
+      width: 70%;
+      text-align: center;
+    }
+    .line{
+      width: 0;
+      height: 20px;
+      border-left: 1px solid #fff ;
+      flex: 1;
+    }
+  }
+  .home_new_7{
+    position: absolute;
+    top: 0px;
+    right: 30px;
+    width: 25%;
+    height: 100%;
+    // border: 1px solid red;
+    background-image: url('../../assets/home/home_new_line.png');
+    background-size: 80% 50%;
+    background-position: 50% 130%;
+    background-repeat: no-repeat;
+    .home_new_logo{
+      width: 50px;
+      height: 50px;
+      position: absolute;
+    }
+    .weibo{
+      left: 45%;
+      bottom: 18%;
+    }
+    .douyin{
+      left: 30%;
+      bottom: 30%;
+    }
+    .wechat{
+      right: 35%;
+      bottom: 32%;
+    }
+    .hongshu{
+      left: 20%;
+      bottom: 38%;
+    }
+    .vieo{
+      right: 38%;
+      bottom: 45%;
+    }
+    .ab{
+      left: 15%;
+      bottom: 55%;
+    }
+    .taobao{
+      left: 40%;
+      bottom: 58%;
+    }
+    .jingdong{
+      left: 10%;
+      bottom: 70%;
+    }
+    .fast_hand{
+      left: 35%;
+      bottom: 73%;
+    }
+  }
+  .home_new_item_text{
+    line-height: 24px;
+    // font-size: 16px;
+    padding: 20px 30px;
+  }
+  .num_bold{
+    font-weight: bold;
+    font-size: 48px;
+    text-align: center;
+  }
 }
 @keyframes text-shadow-pop-tl {
   0% {
